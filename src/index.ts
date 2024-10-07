04d9211542f75bbd1c188f70e3e46af7aafb3298
pi55man <cuinnv@gmail.com>
2024-10-08 00:04:52 +0530

    generat prisma migration, set up basic router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import app from './server.js'
+import app from './server'
 
 const port = 8080
 app.listen(port,()=>{
-        console.log(`listening on ${port}`);
+        console.log(`listening on http://localhost:${port}`);
 })
