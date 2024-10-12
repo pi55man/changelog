ed1e67015e085f7c9584b04676e1b71e592799a7
pi55man <cuinnv@gmail.com>
2024-10-12 14:55:37 +0530

    implement product handlers

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,33 @@
 import {Router} from "express";
-
+import {body, oneOf, validationResult} from 'express-validator';
+import { handleInputErrors } from "./modules/middleware";
+import { createProduct, deleteProduct, getOneProduct, getProducts } from "./handlers/product";
 const router = Router();
 
-router.get('/product',(req, res)=>{
-        res.json({message: "hi"});
+router.get('/product',getProducts)
+router.get('/product/:id',getOneProduct)
+
+router.put('/product/:id', 
+           body("name").isString(),
+           handleInputErrors,
+           (req, res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+                res.status(400);
+                res.json({errors: errors.array()})
+                return;
+        }
+        res.json({message: "no errors"})
+        
 });
 
-router.get('/product/:id',(req, res)=>{
-        res.json({message: req.shhh_secret});
-});
-router.put('/product/:id',(req, res)=>{
+router.post('/product',
+            body("name").isString(),
+            handleInputErrors,
+            createProduct
+           )
 
-});
-router.post('/product',(req, res)=>{
-
-});
-router.delete('/product/:id',(req, res)=>{
-
-});
+router.delete('/product/:id',deleteProduct)
 
 /*
         UPDATES 
@@ -30,10 +40,18 @@ router.get('/update',(req, res)=>{
 router.get('/update/:id',(req, res)=>{
 
 });
-router.put('/update/:id',(req, res)=>{
+router.put('/update/:id',
+           body("title").optional(),
+           body("body").optional(),
+           body("status").isIn(['IN_PROGRESS',"DEPRECATED","SHIPPED"]),
+           body("version").optional(),
+(req, res)=>{
 
 });
-router.post('/update',(req, res)=>{
+router.post('/update',
+           body("title").exists().isString(),
+           body("body").exists().isString(),
+   (req, res)=>{
 
 });
 router.delete('/update/:id',(req, res)=>{
@@ -50,10 +68,16 @@ router.get('/updatepoint',(req, res)=>{
 router.get('/updatepoint/:id',(req, res)=>{
 
 });
-router.put('/updatepoint/:id',(req, res)=>{
-
+router.put('/updatepoint/:id',
+           body("name").optional().isString(),
+           body("description").optional().isString(),
+           (req, res)=>{
 });
-router.post('/updatepoint',(req, res)=>{
+router.post('/updatepoint',
+            body("name").isString(),
+            body("description").isString(),
+            body("updateId").exists().isString(),
+            (req, res)=>{
 
 });
 router.delete('/updatepoint/:id',(req, res)=>{
